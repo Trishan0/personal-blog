e31b2cf1b5a39709728877a7d59c09fc39a72a01
Trishan <sanjanatrishan@gmail.com>
2025-04-15 01:48:58 +0530

    Add admin dashboard route with article loading

---
+++ src/routes/admin.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const adminRouter = Router();
+
+adminRouter.get('/admin/dashboard', (req, res)=>{
+    const articleData = JSON.parse(
+        fs.readFileSync(path.join(__dirname, '../data/article.json'), 'utf-8')
+    )
+
+    res.render('pages/admin/dashboard', {
+        articles: articleData.articles
+    })
+})
