b1ac58b602e559a076a9d911522fda774ed5366f
Trishan <sanjanatrishan@gmail.com>
2025-04-14 22:55:01 +0530

    Add user route and seed article data

---
+++ src/routes/user.mjs
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const userRouter = Router();
+
+
+
+userRouter.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../pages/home.html'));
+})
