eaac6b2d5664dd33bc0188847a166521cfb9d632
Trishan <sanjanatrishan@gmail.com>
2025-04-14 23:13:54 +0530

    Refactor article routes and streamline endpoint logic.

--- src/routes/article.mjs
+++ src/routes/article.mjs
@@ -2,24 +2,17 @@ import { Router } from "express";
 import data from "../data/article.json" with { type: "json" };
 export const articleRouter = Router();
 
-articleRouter.get("/all-articles", (_, res)=>{
-    res.status(200).json({
-        message: "All articles",
-        data:data.articles
-    })
-    }
-)
+articleRouter.get("/article/:id", (req, res)=>{
+    const {id} = req.params;
+    if (!id) return res.status(400).json({message: "Please provide an id"})
 
-articleRouter.get("/id", (req, res)=>{
-    const {id} = req.query;
-    if (!req.query.id) return res.status(400).json({message: "Please provide an id"})
+    const article = data.articles.find(article => article.id === Number(id))
 
-    if ( id !== undefined){
-        const article = data.articles.find(article => article.id === Number(id))
-        return  res.status(200).json({
-            message: "All articles",
-            data:article
-        })
+    if (!article) {
+        return res.status(404).json({message: "Article not found"})
     }
-    res.sendStatus(200 )
-})
+    return res.status(200).json({
+        message: "Article found",
+        data: article
+    });
+});
