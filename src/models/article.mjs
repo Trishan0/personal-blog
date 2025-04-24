afc24fd5482f01c64bbebba0fc61b5b93211d5e3
Trishan <sanjanatrishan@gmail.com>
2025-04-24 15:26:31 +0530

    Add auto-incrementing ID to Article model
    Integrated mongoose-sequence to generate auto-incrementing IDs for the Article model.

--- src/models/article.mjs
+++ src/models/article.mjs
@@ -1,16 +1,25 @@
-import mongoose, {mongo} from 'mongoose';
+import mongoose from 'mongoose';
+import AutoIncrementFactory from 'mongoose-sequence';
+import {APP_CONFIG} from "../config/index.mjs";
+
+const connection = mongoose.createConnection(APP_CONFIG.MONGODB_URI);
+const AutoIncrement = AutoIncrementFactory(connection);
 
 const articleSchema = new mongoose.Schema({
+    id: {
+        type: Number
+    },
     title: {
         type: String,
-        required:[true, "Article Title is Required"],
+        required: [true, "Article Title is Required"],
         trim: true,
     },
-    date:  {
-        type:Date,
-        default: Date.now()
+    date: {
+        type: Date,
+        default: Date.now,
     },
     content: String,
-})
+});
 
+articleSchema.plugin(AutoIncrement, { inc_field: 'id' });
 export const Article = mongoose.model('Article', articleSchema);
