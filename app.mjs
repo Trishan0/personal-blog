c4d1197be9f22755dd4e11483e5d533b3ac75381
Trishan <sanjanatrishan@gmail.com>
2025-04-24 11:38:03 +0530

    Add database connection and middleware for request parsing

--- app.mjs
+++ app.mjs
@@ -4,13 +4,16 @@ import {articleRouter} from "./src/routes/article.mjs";
 import {adminRouter} from "./src/routes/admin.mjs";
 import path from "node:path";
 import {fileURLToPath} from "node:url";
-
+import { connectToDB } from "./src/database/db.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const app = express()
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 app.use(userRouter)
 app.use(articleRouter)
 app.use(adminRouter)
@@ -20,6 +23,11 @@ app.set('view engine', 'ejs')
 
 
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000')
+connectToDB().then(() => {
+    app.listen(4000, () => {
+        console.log('Server is running on port 4000')
+    })
+}).catch(err => {
+    console.error('Failed to connect to the database:', err)
+    process.exit(1)
 })
