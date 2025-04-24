e07b613923816085128eff533d9ea1b4ab765cc6
Trishan <sanjanatrishan@gmail.com>
2025-04-24 18:36:09 +0530

    Refactor article controllers and add getArticleById.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,26 +1,55 @@
 import {Article} from "../models/article.mjs";
 
-
-export const getAllArticles = async (req, res)=>{
+const getArticleById = async (req, res) => {
     try {
-        const allArticles = await Article.find({})
-        if (allArticles?.length > 0) {
-            res.status(200).json({
-                success: true,
-                message: "List of Articles fetched successfully",
-                data: allArticles
-            })
-        } else {
-            res.status(404).json({
-                success: false,
-                message: "No Articles found !",
-
-            })
+        const article = await Article.findOne({ id: req.params.id });
+        if (!article) {
+            return res.status(404).render('pages/article', {
+                article,
+                links: ['/css/pages/new_article.css']
+            });
         }
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: "Something went wrong! Please Try again!"
-        })
+
+        article.content = article.content || "";
+
+        res.render('pages/article', {
+            article,
+            links: ['/css/pages/new_article.css']
+        });
+
+    } catch (e) {
+        res.status(500).render('error', {
+            message: "Server error"
+        });
     }
+};
+const getAllArticles = (viewName = 'pages/home', cssFile) => {
+    return async (req, res) => {
+        try {
+            const articles = await Article.find({});
+            if (articles?.length > 0) {
+                res.render(viewName, {
+                    articles,
+                    links: [cssFile]
+                });
+                console.log(articles);
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "No Articles found!"
+                });
+            }
+        } catch (error) {
+            res.status(500).json({
+                success: false,
+                message: "Something went wrong! Please try again!"
+            });
+        }
+    };
+};
+
+
+export {
+    getAllArticles,
+    getArticleById
 }
