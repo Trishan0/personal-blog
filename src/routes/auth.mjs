414b08dd37a4699899eb42b88f83c901f31461d0
Trishan <sanjanatrishan@gmail.com>
2025-04-29 10:52:27 +0530

    Add login routes and sign-up page rendering

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -1,6 +1,16 @@
 import {Router} from 'express';
-import { registerUser } from "../controllers/auth.controller.mjs";
+import { registerUser, loginUser } from "../controllers/auth.controller.mjs";
+import path from 'path';
+import {adminRouter} from "./admin.mjs"; //
 
 export const authRouter = Router();
 
 authRouter.post('/register', registerUser);
+authRouter.get('/register', (req, res)=>{
+    res.render('pages/sign-up')
+})
+
+adminRouter.get('/login', (req, res)=>{
+    res.render('pages/sign-up')
+})
+adminRouter.post('/login', loginUser);
