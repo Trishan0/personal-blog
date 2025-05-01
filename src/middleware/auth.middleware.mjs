e7050a0f80e289ac05e90aecb1c6c5f7f8aac394
Trishan <sanjanatrishan@gmail.com>
2025-05-01 18:58:23 +0530

    Refactor authMiddleware and add setUserLocals function
    Refactored authMiddleware to remove export inline and introduced setUserLocals for setting userRole in response locals.

--- src/middleware/auth.middleware.mjs
+++ src/middleware/auth.middleware.mjs
@@ -1,6 +1,5 @@
-export const authMiddleware = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     if (req.session && req.session.user) {
-        // Make user info available in route
         req.userInfo = req.session.user;
         return next();
     }
@@ -10,3 +9,13 @@ export const authMiddleware = (req, res, next) => {
         message: "Access Denied"
     });
 };
+
+const setUserLocals = (req, res, next) => {
+    res.locals.userRole = req.session?.user?.role || null;
+    next()
+}
+
+export {
+    authMiddleware,
+    setUserLocals
+}
